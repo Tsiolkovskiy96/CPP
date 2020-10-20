e89e7711f873b752ea517335b1ef124596b51eac
Vyacheslav <tarakanov.ekb@gmail.com>
2020-10-20 21:24:15 +0500

    Start

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>                 // Подключение стандартной библиотеки
+
+#define IT_WORKS "It works!"        // Подстановочная константа
+
+using namespace std;                // Разрешение использования имен из стандартной библиотеки
+
+int main (int argc, char** args)    // Внутренние имена
+{
+    const int SUCCESS = 0;          // Имя константы
+    cout << IT_SAMPLE << endl;      // Обращение к стандартной библиотеке
+    return SUCCESS;                 // Опреация возврата
+}
