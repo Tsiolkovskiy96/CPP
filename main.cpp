be3a21ec2699363cb12f9ba4e563184c68de764f
Vyacheslav <tarakanov.ekb@gmail.com>
2020-10-27 15:40:45 +0500

    271020

--- main.cpp
+++ main.cpp
@@ -317,6 +317,7 @@ int main(int argc, char** args)
 }*/
 
 // Урок 3.7
+/*
 #include <iostream>
 
 using namespace std;
@@ -325,6 +326,12 @@ int main(int argc, char** args)
 {
     int a;
     cin >> a;
+    try {
+       if(a == 0) throw "Division by zero\n";
+       cout << 4 / a << endl;
+    }  catch (char const* e) {
+       cout << e;
+    }
     cout << 4 / a;
     return 0;
-}
+} */
